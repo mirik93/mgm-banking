2f41b1c357f4e6e2ef7310634fda1ce8c8745b33
mirik93 <mirikgritsak@gmail.com>
2020-01-17 13:31:20 -0800

    initial commit

--- mgmbanking/__init__.py
+++ mgmbanking/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask
 
-#Config imports
+# Config imports 
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-
-#import for flask login
-from flask_login import LoginManager
 
 app = Flask(__name__)
 
@@ -15,8 +12,4 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
 
-#login flow config
-login = LoginManager(app)
-login.login_veiw = 'login' #this says what page to load
-
-from mgmbanking import routes
+from mgmbanking import routes, models
